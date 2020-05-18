3ed9c0d1463715eb5f961ff99005a72aa8d0d6a3
Francisco Giovanildo Teixeira de Souza <giovanildos@gmail.com>
2020-05-18 02:07:37 -0300

    removendo arquivos desnecessários e iniciando a modelagem das frequencias

---
+++ src/main/java/com/giovanildo/models/PlanoDeTrabalho.java
@@ -0,0 +1,33 @@
+package com.giovanildo.models;
+
+import java.util.Collection;
+
+public class PlanoDeTrabalho {
+	private int id;
+	private String descricao;
+	private Collection<FrequenciaMensal> frequenciasMensais;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Collection<FrequenciaMensal> getFrequenciasMensais() {
+		return frequenciasMensais;
+	}
+
+	public void setFrequenciasMensais(Collection<FrequenciaMensal> frequenciasMensais) {
+		this.frequenciasMensais = frequenciasMensais;
+	}
+}
